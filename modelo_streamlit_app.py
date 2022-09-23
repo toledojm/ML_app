2038cfd72e97b404be5b00ee5590341963caf4b8
jose toledo <104653070+toledojm@users.noreply.github.com>
2022-09-23 19:10:57 -0300

    Add files via upload

---
+++ modelo_streamlit_app.py
@@ -0,0 +1,56 @@
+import pandas as pd
+import numpy as np
+from PIL import Image
+from joblib import dump,load
+import streamlit as st
+
+
+st.set_page_config(page_title="streamlit", layout="wide")
+
+primaryColor="#F63366"
+backgroundColor="#FFFFFF"
+texColor="#F0F2F6"
+font="sans serif"
+
+st.markdown("<h1 style='text-align: center; color:black;'> ML model deployment </h1>", unsafe_allow_html=True)
+st.markdown("<br></br>",unsafe_allow_html=True)
+
+image=Image.open('iris.jpg')
+st.image(image)
+
+with st.sidebar:
+    clasificador =st.selectbox(label='clasificador', options=('Decision tree calssifier', 'k-nn'))
+    st.markdown("<br></br>",unsafe_allow_html=True)
+
+    sepal_length=st.slider(label='sepal length (cm):',min_value=2.0,max_value=9.0,step=0.05)
+    sepal_width=st.slider(label='sepal length (cm):',min_value=1.0,max_value=6.0,step=0.05)
+    petal_length=st.slider(label='petal length (cm):',min_value=0.0,max_value=8.0,step=0.05)
+    petal_width=st.slider(label='petal width (cm):',min_value=0.0,max_value=8.0,step=0.05)
+    
+def classifier(clasificador):
+    if clasificador=='Decision tree calssifier':
+        tree_classifier=load('clf_dtc_model.py')
+        resultado=tree_classifier.predict([[sepal_length,sepal_width,petal_length,petal_width]])
+    elif clasificador=='k-nn':
+        knn_classifier=load('clf_knn_model.py')
+        resultado=knn_classifier.predict([[sepal_length,sepal_width,petal_length,petal_width]])
+    else:
+        resultado=None
+    return resultado
+
+col1,col2,col3=st.columns(3)
+
+flecha=Image,open('flecha.png')
+
+if st.button('clasifique por favor'):
+    clase=int(classifier(clasificador))
+    if clase==0:
+        col1.image(flecha)
+    elif clase==1:
+        col2.image(flecha)
+    elif clase==3:
+        col3.image(flecha)
+    else:
+        pass
+else:
+    st.write('elija clasificador y parámentros')
